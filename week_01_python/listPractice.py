d1b8fa7c788f7fe18ba803b57c8c0f0237714519
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-09-08 19:23:25 -0400

    printed list of lists

--- week_01_python/listPractice.py
+++ week_01_python/listPractice.py
@@ -5,3 +5,9 @@ print(list)
 # creating a list of lists
 list2 = [[0, 0, 0, 0], [0, 0, 0, 0]]
 print(list2)
+
+# printing list using for loops
+for i in range(len(list2)):
+    for j in range(len(list2[i])):
+        print(list2[i][j], end = ' ')
+    print()
