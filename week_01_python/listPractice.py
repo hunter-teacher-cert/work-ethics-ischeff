f4c9b7f91ff6d732e2eecff145c7a83bc354e5ca
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-09-08 19:26:23 -0400

    added another type of for loop

--- week_01_python/listPractice.py
+++ week_01_python/listPractice.py
@@ -11,3 +11,10 @@ for i in range(len(list2)):
     for j in range(len(list2[i])):
         print(list2[i][j], end = ' ')
     print()
+
+# printing the list using for loops in a different way
+# (credit to snakify's python tutorial!)
+for row in list2:
+    for elem in row:
+        print(elem, end = ' ')
+    print()
