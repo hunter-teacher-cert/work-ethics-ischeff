9daca6999d819fadbd0e1b83866597f04568fc32
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-09-21 22:16:30 -0400

    started program

---
+++ week_01_python/inventory.py
@@ -0,0 +1,6 @@
+# this program is designed to demonstrate some proficiency
+# with dictionaries. Inspired partly by my Friday-night D and D games,
+# and also by the Dictionaries chapter in Automate the Boring Stuff,
+# which suggests writing a program to store the inventory for a fantasy game.
+def displayInventory(inventory):
+    
