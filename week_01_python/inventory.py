83bc26a97082a434f10e35694642b9dcde8ff647
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-09-21 22:32:14 -0400

    can print dictionary

--- week_01_python/inventory.py
+++ week_01_python/inventory.py
@@ -2,11 +2,18 @@
 # with dictionaries. Inspired partly by my Friday-night D and D games,
 # and also by the Dictionaries chapter in Automate the Boring Stuff,
 # which suggests writing a program to store the inventory for a fantasy game.
-my_stuff = {'axe': 1 + 'cloak': 2 + 'arrow': 14 + 'scroll': 5}
+my_stuff = {'axe': 1, 'cloak': 2, 'arrow': 14, 'scroll': 5}
 
 def displayInventory(inventory):
     print('Inventory:')
     total_items = 0
     for k, v in inventory.items():
-        print(str(v) + k)
-        # total_items += v.get(item, 0)
+        print(str(v) + ' ' + k)
+        total_items += v
+    print('Total items: ' + str(total_items))
+
+def addItem(inventory):
+    
+
+if __name__ == '__main__':
+    displayInventory(my_stuff)
