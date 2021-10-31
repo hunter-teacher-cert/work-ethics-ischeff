13a40729b8f57f3552e8e10c6faefed99c377670
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-10-31 14:33:13 -0400

    added code template, need to modify regex

---
+++ week_07_regex/regex.py
@@ -0,0 +1,21 @@
+import re
+
+
+def find_name(line):
+    pattern = r"\d{1,2}/\d{1,2}/\d{2,4}"
+    result = re.findall(pattern,line)
+
+    pattern = r"(October|Oct|November|Nov)( \d{1,2}, \d{4})"
+    result = result + re.findall(pattern,line)
+    return result
+
+
+#result = find_date("10/15/2023 is a October 13, 2025 date as is 1/23/19")
+#print(result)
+
+f = open("names.txt")
+for line in f.readlines():
+    #print(line)
+    result = find_name(line)
+    if (len(result)>0):
+        print(result)
